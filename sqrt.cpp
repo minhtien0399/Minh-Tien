afccb3f1d35ec5487d761afd61b8734b0f3bf387
minhtien0399 <nguyenminhtien0399@gmail.com>
2018-02-04 22:58:30 +0700

    lad4bai1

---
+++ sqrt.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+double SQRT(double n);
+int main()
+{
+	int n;
+	char a;
+	do
+	{
+		cout<<"Enter a valuer: ";
+		cin>>n;
+		cout<<"Square root of "<<n<<" is: "<< SQRT(n)<<"\n";
+		cout<<"Continue? (y/n): ";
+		cin>>a;
+	}
+	while (a != 'n');
+	return 0;
+}
+double SQRT(double n)
+{
+	return sqrt(n);
+}
