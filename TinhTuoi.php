0fa6c5b508f65211038d31d5bc6876ff11c05dd0
minhtien0399 <36031876+minhtien0399@users.noreply.github.com>
2023-02-02 16:45:23 +0700

    Create TinhTuoi.php

---
+++ TinhTuoi.php
@@ -0,0 +1,13 @@
+<?php
+$m; //tuoi me
+$n ;//tuoi con
+//me hon con 28
+$a = 28; //$m - $n = 28
+//tuoi me gap 5 lan con
+$b = 5; //$m = $n*5
+if ( $m = $a + $n)
+{
+    $n = $a / ($b - 1);
+    $m = $n + $a;
+}
+echo "Tuoi me la: $m \nTuoi con la: $n";
